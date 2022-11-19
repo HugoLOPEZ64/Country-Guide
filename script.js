520db071dac9deef0f7fd285f6fc64a3d73f4af6
Hugo LOPEZ <hugo.lopez002@gmail.com>
2022-11-19 13:25:35 +0100

    change styles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
-    let countryName = "India";
+    let countryName = countryInp.value;
     let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalUrl);
     fetch(finalUrl)
     .then((response) => response.json())
-    .then((data) => {
+    .then((data) => { /*
         console.log(data[0]);
         console.log(data[0].capital[0]);
         console.log(data[0].flags.svg);
@@ -14,7 +14,7 @@ searchBtn.addEventListener("click", () => {
         console.log(data[0].continents[0]);
         console.log(Object.keys(data[0].currencies)[0]);
         console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
+        console.log(Object.values(data[0].languages).toString().split(",").join(", ")); */
 
         result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
@@ -50,6 +50,13 @@ searchBtn.addEventListener("click", () => {
         </div>
     </div>
         `;
+    }).catch(()=>{
+        if(countryName.length == 0){
+            result.innerHTML = `<h3>please type a country name 👆`
+        }
+        else{
+            result.innerHTML = `<h3>please enter a valid country name 🤓 </h3>`;
+        }
     });
 
 });
